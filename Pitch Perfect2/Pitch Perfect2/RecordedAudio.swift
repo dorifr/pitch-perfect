09f94a516c87a28b648fb0942ff640fba9672eb0
dorifr <dori.frost@gmail.com>
2015-03-19 19:12:12 +0200

    Committed by Udacity

---
+++ Pitch Perfect2/Pitch Perfect2/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect2
+//
+//  Created by Dori Frost on 3/15/15.
+//  Copyright (c) 2015 Dori.Frost. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    
+    var filePathURL: NSURL!
+    var title: String!
+}
